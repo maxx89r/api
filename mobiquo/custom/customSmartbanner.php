b389a69342eac5448e16054f902e7b53fb672188
TomWu <phperwuhan@gmail.com>
2013-04-17 15:35:38 +0800

    add smartbanner basic code on framework level
    Signed-off-by: TomWu <phperwuhan@gmail.com>

---
+++ mobiquo/custom/customSmartbanner.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * custom smartbanner info
+ * 
+ * @since  2013-4-17
+ * @author Wu ZeTao <578014287@qq.com>
+ */
+Class MbqSmartbanner {
+    public static $MBQ_SMARTBANNER_IS_MOBILE_SKIN = 0;  //0 or 1
+    public static $MBQ_SMARTBANNER_APP_IOS_ID = 0;  //set to 0 if not byo
+    public static $MBQ_SMARTBANNER_APP_ANDROID_URL = '';  //set to '' if not byo
+    public static $MBQ_SMARTBANNER_APP_KINDLE_URL = '';  //set to '' if not byo
+    public static $MBQ_SMARTBANNER_APP_BANNER_MESSAGE = '';  //set to '' if not byo
+    public static $MBQ_SMARTBANNER_APP_FORUM_NAME = '';  //required
+    public static $MBQ_SMARTBANNER_APP_LOCATION_URL = '';  //optional
+    
+    public static $MBQ_SMARTBANNER_TAPATALKDIR = 'mobiquo';
+}
+
+?>
